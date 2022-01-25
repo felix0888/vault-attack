602f5b5c6fc9ca70289acfb2093c57fa17681e2b
felix0888 <felixnan88@gmail.com>
2022-01-25 09:32:13 -0500

    Vault contract done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  console.log(
+    "Deploying contracts with the account: ", deployer.address
+  );
 
-  await greeter.deployed();
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
 
-  console.log("Greeter deployed to:", greeter.address);
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await Vault.deploy();
+  console.log("Vault address: ", await vault.address);
+  console.log("Account balance after Vault deploy: ", (await deployer.getBalance()).toString());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ test/Vault.test.js
@@ -0,0 +1,41 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Vault", function () {
+  let Vault, vault;
+  let alice, bob, carol, signers;
+  let pwdBytes;
+  const pwdStr = "vault is locked with password";
+
+  beforeEach(async function() {
+    [alice, bob, carol, signers] = await ethers.getSigners();
+    pwdBytes = stringToBytes32(pwdStr);
+    Vault = await ethers.getContractFactory("Vault");
+    vault = await Vault.deploy(pwdBytes);
+  });
+
+  describe("deployment", function() {
+    it("should set locked", async function() {
+      expect(await vault.locked()).to.equal(true);
+    });
+  });
+
+  describe("#unlock", function() {
+    it("should not unlock if user tries with invalid password", async function() {
+      await vault.unlock(stringToBytes32("wrong password"));
+      expect(await vault.locked()).to.equal(true);
+    });
+
+    it("should unset locked if user tries with valid password", async function() {
+      await vault.unlock(pwdBytes);
+      expect(await vault.locked()).to.equal(false);
+    });
+  });
+
+  var Zeros = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  function stringToBytes32(str) {
+    let bytes = ethers.utils.toUtf8Bytes(str);
+    if (bytes.length > 31) { throw new Error('too long'); }
+    return ethers.utils.concat([bytes, Zeros]).slice(0, 32);
+  }
+});
